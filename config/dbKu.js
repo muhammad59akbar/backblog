445e13e48dcbc49a931b6cb9998e3446c13c4d3b
Muhammad Rizki Akbar <muhammad59akbar@gmail.com>
2023-04-29 15:41:46 +0700

    config

--- config/dbKu.js
+++ config/dbKu.js
@@ -4,7 +4,7 @@ import mysql2 from "mysql2";
 const dbKu = new Sequelize(
   "b8v3wjmisnumx1mfwfnk",
   "uk7sb6yuq373qlv5",
-  "uk7sb6yuq373qlv5",
+  "An4peiZdtZid0VQ8fsV8",
   {
     host: "b8v3wjmisnumx1mfwfnk-mysql.services.clever-cloud.com",
     dialect: "mysql",
