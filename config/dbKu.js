0f79bc6c0d103ce78c3d4827bd97afdc6081e1a6
Muhammad Rizki Akbar <muhammad59akbar@gmail.com>
2023-04-29 12:58:13 +0700

    add anymore

--- config/dbKu.js
+++ config/dbKu.js
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
 const dbKu = new Sequelize(
   "freedb_dbblogku",
@@ -7,6 +8,7 @@ const dbKu = new Sequelize(
   {
     host: "sql.freedb.tech",
     dialect: "mysql",
+    dialectModule: mysql2,
   }
 );
 
