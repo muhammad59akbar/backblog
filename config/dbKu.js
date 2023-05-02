7ae766f079386baa01c2f971015fd007c0333b48
Muhammad Rizki Akbar <muhammad59akbar@gmail.com>
2023-05-02 10:36:50 +0700

    deploy vercell

--- config/dbKu.js
+++ config/dbKu.js
@@ -1,14 +1,14 @@
 import { Sequelize } from "sequelize";
-import mysql2 from "mysql2";
 
 const dbKu = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || "b8v3wjmisnumx1mfwfnk",
+  process.env.DB_USERNAME || "uk7sb6yuq373qlv5",
+  process.env.DB_PASSWORD || "An4peiZdtZid0VQ8fsV8",
   {
-    host: process.env.DB_HOST,
+    host:
+      process.env.DB_HOST ||
+      "b8v3wjmisnumx1mfwfnk-mysql.services.clever-cloud.com",
     dialect: "mysql",
-    dialectModule: mysql2,
   }
 );
 
